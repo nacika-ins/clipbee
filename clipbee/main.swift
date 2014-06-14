cb72b859d7235988d362884e5d6ffcda1900d5d3
nacika <nacika.inscatolare@gmail.com>
2014-06-14 13:49:49 +0900

    First Commit.

---
+++ clipbee/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  clipbee
+//
+//  Created by nacika on 2014/06/14.
+//  Copyright (c) 2014年 nacika. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
