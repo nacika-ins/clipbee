1099f85bc213e28d7d7a1267872394f4b9006292
nacika <nacika.inscatolare@gmail.com>
2014-07-06 18:58:23 +0900

    一時的にコミット

--- clipbee/Model.swift
+++ clipbee/Model.swift
@@ -9,5 +9,38 @@
 import Cocoa
 
 class Model: NSObject {
+    
+    /// <##>
+    //-------------------------------------------------------------------------------
+    init() {
+        super.init()
+    }
+    
+    /// 作成されたインスタンスを保持する
+    //-------------------------------------------------------------------------------
+    class var instances: NSMutableArray {
+        struct Instances {
+            static let instances: NSMutableArray = []
+        }
+        return Instances.instances
+    }
+    
+    /// 全てのインスタンスを取得
+    //-------------------------------------------------------------------------------
+    class func all () -> NSArray {
+    
+        var array = self.instances
+        
+        return array
+        
+    }
+    
+    //// インスタンスの追加
+    //-------------------------------------------------------------------------------
+    class func create () {
+    
+        println(self.instances)
+        
+    }
 
 }
