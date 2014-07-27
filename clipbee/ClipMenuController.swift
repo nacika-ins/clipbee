f7d9a3ba65e6219a3d2f62480db58b374d4ab52e
nacika <nacika.inscatolare@gmail.com>
2014-07-28 02:30:22 +0900

    WIP

---
+++ clipbee/ClipMenuController.swift
@@ -0,0 +1,13 @@
+//
+//  ClipMenuController.swift
+//  clipbee
+//
+//  Created by nacika on 2014/07/27.
+//  Copyright (c) 2014年 nacika. All rights reserved.
+//
+
+import Cocoa
+
+class ClipMenuController: Controller {
+
+}
